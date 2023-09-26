8493d69a6f02cb405e94e623f58ca108ad803342
Chrisna Wisnu <145436426+Chrisnawisnu88@users.noreply.github.com>
2023-09-26 14:49:16 +0700

    Create contoh1.php
    perkenalan diri

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+kelas  Contoh1  memperluas  CI_Controller
+{
+     indeks fungsi  publik ()
+    {
+        echo " <h1>Perkenalan Diri</h1> ";
+        echo " Nama : Chrisna Wisnu Trianing Bagar <br> ";
+        gema " Nim : 12220229 <br> ";
+        echo " Kelas: 12.3A.05 <br> ";
+        echo " TTL : Bekasi, 10 Agustus 2001 <br> ";
+    }
+}
